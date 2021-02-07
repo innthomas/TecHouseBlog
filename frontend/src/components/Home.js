1462294284ce56cd006df4fcda18dbea52becd95
Innocent Thomas <innthomas00158@gmail.com>
2021-02-07 18:57:14 +0100

    landing page edit

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const home = () => (
     <div className='container'>
         <div className="jumbotron mt-5">
-            <h1 className="display-4">Welcome to Blog Lyfe!</h1>
-            <p className="lead">We make all kinds of awesome blog about various topics.</p>
+            <h1 className="display-4">Welcome to TecHouse!</h1>
+            <p className="lead">We make all kinds of awesome news updae about technology related topics.</p>
             <hr className="my-4" />
             <p>Click the button below to check out our awesome blog.</p>
             <Link className="btn btn-primary btn-lg" to='/blog' role="button">Check out our Blog</Link>
